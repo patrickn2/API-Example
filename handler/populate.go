e6e54763dedae54cfbda13e9062f61bb13d85d33
Patrick Nogueira <patricknn@gmail.com>
2024-08-08 17:17:47 -0300

    feat: Improving http Canceling

--- handler/populate.go
+++ handler/populate.go
@@ -17,7 +17,8 @@ import (
 
 func (h *Handler) Populate(ctx *gin.Context) {
 	cx, cancel := context.WithCancel(context.Background())
-	go checkConnectionStatus(cancel, ctx)
+	done := make(chan bool)
+	go checkConnectionStatus(cancel, ctx, done)
 	var UserChan = make(chan *[]schema.User)
 	var wg sync.WaitGroup
 	totalAPICalls := 5
@@ -30,33 +31,38 @@ func (h *Handler) Populate(ctx *gin.Context) {
 	for i := 0; i < totalAPICalls; i++ {
 		u := <-UserChan
 		if u == nil {
-			ctx.Status(500)
-			return
+			continue
 		}
 		users = append(users, *u...)
 	}
+	if len(users) == 0 {
+		ctx.JSON(http.StatusFailedDependency, map[string]int{"rows_affected": 0})
+	}
 	rows, err := model.InsertNewUsers(h.db, &users)
 	if err != nil {
-		fmt.Printf("error inserting users %v\n", err)
+		log.Printf("error inserting users %v\n", err)
+		done <- true
 		ctx.Status(http.StatusInternalServerError)
 		return
 	}
+	done <- true
 	ctx.JSON(http.StatusOK, map[string]int{"rows_affected": rows})
 }
 
-func checkConnectionStatus(cancel context.CancelFunc, ctx *gin.Context) {
+func checkConnectionStatus(cancel context.CancelFunc, ctx *gin.Context, done chan bool) {
 	t := time.NewTicker(500 * time.Millisecond)
 	defer t.Stop()
 
-	for i := 0; i < 10; i++ {
+	for {
 		select {
 		case <-t.C:
 			continue
+		case <-done:
+			return
 		case <-ctx.Request.Context().Done():
-			fmt.Println("Connection Aborted")
-			cancel()
-			ctx.AbortWithError(http.StatusGone, ctx.Request.Context().Err())
-			// client gave up
+			log.Println("Connection Aborted")
+			cancel() // Cancel all http requests
+			ctx.Abort()
 			return
 		}
 	}
@@ -64,44 +70,42 @@ func checkConnectionStatus(cancel context.CancelFunc, ctx *gin.Context) {
 
 func getRandomUser(ctx context.Context, u chan *[]schema.User, wg *sync.WaitGroup, try int) {
 	if try == 0 {
-		fmt.Printf("server is not responding as expected after some requests")
+		log.Println("server is not responding as expected after some requests")
 		u <- nil
 		wg.Done()
 		return
 	}
 	req, err := http.NewRequestWithContext(ctx, "GET", "https://randomuser.me/api/?results=1000", http.NoBody)
 	if err != nil {
-		fmt.Println("error creating new request", err)
+		log.Println("error creating new request", err)
 		u <- nil
+		wg.Done()
 		return
 	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println("error getting user", err)
+		log.Println("error getting user", err)
 		u <- nil
+		wg.Done()
 		return
 	}
 	defer resp.Body.Close()
 
 	var randomUsers schema.RandomUser
 
+	delay := time.Millisecond * time.Duration((rand.Float64()*200)+400+(5-float64(try)))
 	if resp.StatusCode == http.StatusTooManyRequests {
-		if ctx.Err() != nil {
-			fmt.Println("Request Canceled")
-			return
-		}
-		delay := int(rand.Float64()*100) + 400
-		log.Printf("Too Many Requests. Trying again in %d ms", delay)
-		time.Sleep(time.Millisecond * time.Duration(delay))
-		getRandomUser(ctx, u, wg, try)
+		log.Printf("Too Many Requests. Trying again in %d ms\n", delay)
+		time.Sleep(delay)
+		getRandomUser(ctx, u, wg, try-1)
 		return
 	}
 
 	err = json.NewDecoder(resp.Body).Decode(&randomUsers)
 
 	if err != nil {
-		log.Printf("error decoding json %v - Fetch Failed - Trying Again... ", err)
-		time.Sleep(time.Millisecond * (time.Duration(int(rand.Float64()*100)) + 100))
+		log.Printf("error decoding json %v - Fetch Failed - Trying Again... \n", err)
+		time.Sleep(delay)
 		getRandomUser(ctx, u, wg, try-1)
 		return
 	}
