18c1c4bf2223911df669b9885b760bb15a2fc0ab
Patrick Nogueira <patricknn@gmail.com>
2024-08-22 10:41:03 -0300

    fix: drop Index names

--- migrations/1724186538_init.down.sql
+++ migrations/1724186538_init.down.sql
@@ -1,7 +1,7 @@
 -- Migration Down: init
 
-DROP INDEX idx_users_created_at_desc_email;
-DROP INDEX idx_users_created_at_email;
+DROP INDEX users_email_trgm_idx;
+DROP INDEX users_created_at_idx;
 
 DROP TABLE users;
 DROP DATABASE clerk;
